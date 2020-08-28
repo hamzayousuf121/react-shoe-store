ab57434ed418aad9f9f049408179af776896c424
Hamza Yousuf <hamzayousuf121@gmail.com>
2020-08-28 16:18:16 +0500

    Deploy App

---
+++ build/precache-manifest.801b966cb53cc4fd127b4a7bcf965fa3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe90b061c67b306887b8d8808399743",
+    "url": "/react-shoe-store/index.html"
+  },
+  {
+    "revision": "9867f4aca6e0fe5f1781",
+    "url": "/react-shoe-store/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "78bd2eba3f91e560d865",
+    "url": "/react-shoe-store/static/css/main.a94a918c.chunk.css"
+  },
+  {
+    "revision": "9867f4aca6e0fe5f1781",
+    "url": "/react-shoe-store/static/js/2.da9a91a3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-shoe-store/static/js/2.da9a91a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78bd2eba3f91e560d865",
+    "url": "/react-shoe-store/static/js/main.5571d6b9.chunk.js"
+  },
+  {
+    "revision": "47d4a8457a7dd4c06b11",
+    "url": "/react-shoe-store/static/js/runtime-main.ae2396cc.js"
+  },
+  {
+    "revision": "b5e728d36f2b5e1bfa46df798b45999e",
+    "url": "/react-shoe-store/static/media/download (1).b5e728d3.png"
+  },
+  {
+    "revision": "89c06bda59982cdd8eb77b48af01d4d8",
+    "url": "/react-shoe-store/static/media/download (2).89c06bda.png"
+  },
+  {
+    "revision": "c2a26c7f570878db56b93d290d098624",
+    "url": "/react-shoe-store/static/media/download.c2a26c7f.png"
+  },
+  {
+    "revision": "8d8b2dfded49d2d7b1db635591879f60",
+    "url": "/react-shoe-store/static/media/img1.8d8b2dfd.png"
+  },
+  {
+    "revision": "279fe702b3e6191b928596b4fcb26eea",
+    "url": "/react-shoe-store/static/media/logo1.279fe702.png"
+  },
+  {
+    "revision": "f69290a54850644876b4381ab12e6a03",
+    "url": "/react-shoe-store/static/media/shoe1.f69290a5.png"
+  }
+]);
